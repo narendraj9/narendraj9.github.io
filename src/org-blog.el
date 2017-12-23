9025be94247c578013ed48d0013c40fdfccd648c
Narendra Joshi <narendraj9@vicarie.in>
2017-12-23 23:23:54 +0530

    Add some information to index.html

--- src/org-blog.el
+++ src/org-blog.el
@@ -53,7 +53,7 @@ Argument `PROJECT-PLIST' contains information about the current project."
 	<p> Built with
 		<svg id=\"i-heart\" viewBox=\"0 0 32 32\">
     		<path d=\"M4 16 C1 12 2 6 7 4 12 2 15 6 16 8 17 6 21 2 26 4 31 6 31 12 28 16 25 20 16 28 16 28 16 28 7 20 4 16 Z\"/>
-	    </svg> and
+	    </svg> in
         <img id=\"i-emacs\" src=\"/assets/images/emacs.svg\">
 	</p>
   </footer>
