6b53a8f6d00f5ee82fca088328e23d4e407d46df
Narendra Joshi <narendraj9@vicarie.in>
2018-09-09 04:03:33 +0530

    Update GA tracking code

--- src/org-blog.el
+++ src/org-blog.el
@@ -83,14 +83,12 @@
   </footer>
 
   <!-- Google Analytics -->
+  <script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-55966581-1\"></script>
   <script>
-   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-   a('create', 'UA-55966581-1', 'auto');
-   ga('require', 'displayfeatures');
-   ga('send', 'pageview');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-55966581-1');
   </script>
 
   <script type=\"text/javascript\" src=\"/assets/js/custom.js\"> </script>
